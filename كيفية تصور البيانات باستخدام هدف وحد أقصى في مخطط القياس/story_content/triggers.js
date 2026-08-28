e61930d04750ec18529de00dbb0e4427248bcb97
mohammadelmedawar <Mohammad.ElMedawar@gmail.com>
2026-08-28 15:45:48 +0300

    Upload

---
+++ كيفية تصور البيانات باستخدام هدف وحد أقصى في مخطط القياس/story_content/triggers.js
@@ -0,0 +1,305 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5gQuCZVYb3l":
+        Script1();
+        break;
+      case "5ojzboAwrmL":
+        Script2();
+        break;
+      case "6jwyn428nEh":
+        Script3();
+        break;
+      case "6n0au3hAIlW":
+        Script4();
+        break;
+      case "6VPAhMZrnTW":
+        Script5();
+        break;
+      case "6USZd8oK7Mg":
+        Script6();
+        break;
+      case "5oEZWBO2xEp":
+        Script7();
+        break;
+      case "5ms7OSSjaAd":
+        Script8();
+        break;
+      case "6LlsKbc6LUr":
+        Script9();
+        break;
+      case "604EGUYYOwp":
+        Script10();
+        break;
+      case "6OrpH8tqUwS":
+        Script11();
+        break;
+      case "65DN93I3MKU":
+        Script12();
+        break;
+      case "5fLAOgHE42I":
+        Script13();
+        break;
+      case "6WH4U572JTO":
+        Script14();
+        break;
+      case "5aX98QhpsyL":
+        Script15();
+        break;
+      case "6I4hKHyU8U5":
+        Script16();
+        break;
+      case "65aLFZjU8NF":
+        Script17();
+        break;
+      case "6qdhzLP8aIc":
+        Script18();
+        break;
+      case "5gpfD6r1vk0":
+        Script19();
+        break;
+      case "6OL7mBdf7jG":
+        Script20();
+        break;
+      case "6WCRtlbBCj4":
+        Script21();
+        break;
+      case "5pDzlHyKctc":
+        Script22();
+        break;
+      case "5gN5PK6aQje":
+        Script23();
+        break;
+      case "6gO9M9uI1zg":
+        Script24();
+        break;
+      case "5uWV2XJvaRP":
+        Script25();
+        break;
+      case "5bdLz7Z4Ide":
+        Script26();
+        break;
+      case "6S502KrE95t":
+        Script27();
+        break;
+      case "5vgK65Z06tr":
+        Script28();
+        break;
+      case "62rSbd1mWdu":
+        Script29();
+        break;
+      case "6pt2jnI81aK":
+        Script30();
+        break;
+      case "5owP2rWLVZG":
+        Script31();
+        break;
+      case "61OQXnZJYHJ":
+        Script32();
+        break;
+      case "6mc6m95ukkx":
+        Script33();
+        break;
+      case "5lhrj1lMfao":
+        Script34();
+        break;
+      case "6D8uumjn26W":
+        Script35();
+        break;
+      case "696qkNLYSwP":
+        Script36();
+        break;
+      case "5uTm4MxKLr8":
+        Script37();
+        break;
+      case "6bOmB0XWCDe":
+        Script38();
+        break;
+      case "5sPShrMABFY":
+        Script39();
+        break;
+      case "60IwiQxEW3K":
+        Script40();
+        break;
+      case "5fDh2QYmYO9":
+        Script41();
+        break;
+      case "5aoXQfMW9e2":
+        Script42();
+        break;
+      case "6Lq1bVeJogu":
+        Script43();
+        break;
+      case "5ju7f6RZws2":
+        Script44();
+        break;
+      case "5e2X0zNHSVQ":
+        Script45();
+        break;
+      case "6BQzgLlVJ7M":
+        Script46();
+        break;
+      case "5x9n7dKsORN":
+        Script47();
+        break;
+      case "5uTh8iYJFbk":
+        Script48();
+        break;
+      case "69CX4qXVzde":
+        Script49();
+        break;
+      case "5a6DVhNbrd8":
+        Script50();
+        break;
+      case "5puxnNRS66p":
+        Script51();
+        break;
+      case "5gM9I8Xcvs0":
+        Script52();
+        break;
+      case "5yVaK40BQie":
+        Script53();
+        break;
+      case "5YrZ7FnZi2k":
+        Script54();
+        break;
+      case "6IdKLTbzzTo":
+        Script55();
+        break;
+      case "6DWrJ67nYNm":
+        Script56();
+        break;
+      case "6IjObeSARoN":
+        Script57();
+        break;
+      case "5rSQ5j3GI7J":
+        Script58();
+        break;
+      case "6Vrw64zNvmx":
+        Script59();
+        break;
+      case "5cKS7wfQKGF":
+        Script60();
+        break;
+      case "6ANk9zvNABG":
+        Script61();
+        break;
+      case "5bSGBXTxAWB":
+        Script62();
+        break;
+      case "5aot4bHbw0A":
+        Script63();
+        break;
+      case "6f82Bfdjp66":
+        Script64();
+        break;
+      case "6hR6Vy3TrNL":
+        Script65();
+        break;
+      case "5XHTw15Tih6":
+        Script66();
+        break;
+      case "6I8bmdANUPw":
+        Script67();
+        break;
+      case "5Wkn5J0av6K":
+        Script68();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5Y63KcUs1uS');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6Ogkg8PmM6S';
+const shakeAmount = 2;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {translate: '0 0' }, 
+{translate: `-${shakeAmount}px 0` }, 
+{translate: '0 0' }, 
+{translate: `${shakeAmount}px 0` }, 
+{translate: '0 0' }, 
+{translate: `-${shakeAmount}px 0` }, 
+{translate: '0 0' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  player.once(() => {
+const target = object('6TLwLmIhnMm');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5xge0hmFItC';
+const shakeAmount = 2;
+const delay = 1000;
+addToTimeline(
+target.animate(
+[ {translate: '0 0' }, 
+{translate: `-${shakeAmount}px 0` }, 
+{translate: '0 0' }, 
+{translate: `${shakeAmount}px 0` }, 
+{translate: '0 0' }, 
+{translate: `-${shakeAmount}px 0` }, 
+{translate: '0 0' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script3 = function()
+{
+  player.once(() => {
+const target = object('5Z5dxg4MaoA');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6ejw4EmzKQO';
+const shakeAmount = 2;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {translate: '0 0' }, 
+{translate: `-${shakeAmount}px 0` }, 
+{translate: '0 0' }, 
+{translate: `${shakeAmount}px 0` }, 
+{translate: '0 0' }, 
+{translate: `-${shakeAmount}px 0` }, 
+{translate: '0 0' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+};
